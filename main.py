76cd946a835dcd32f0ab8d9649a236b089af37e6
Leon Liu <leonliu@Leons-Laptop.local>
2025-02-17 22:40:29 -0800

    Added middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import notes, upload
 from auth import generate_guest_token, verify_token
 
@@ -7,6 +8,15 @@ app = FastAPI(title="AI Notes", description="AI-powered note-taking assistant", 
 
 app.include_router(notes.router, prefix="/api", tags=["notes"],dependencies=[Depends(verify_token)])
 app.include_router(upload.router, prefix="/files", tags=["uploads"],dependencies=[Depends(verify_token)])
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (adjust if needed)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/get-token/")
 def get_guest_token():
